5801e34ab2b0be4a901725dc9b80fdeb91d35b4b
aaronshan <shanruifeng@gmail.com>
2016-07-11 15:05:04 +0800

    fix bugs for dayOfWeek(date)

--- src/main/java/cc/shanruifeng/functions/udfs/scalar/date/DayOfWeekFunction.java
+++ src/main/java/cc/shanruifeng/functions/udfs/scalar/date/DayOfWeekFunction.java
@@ -2,14 +2,19 @@ package cc.shanruifeng.functions.udfs.scalar.date;
 
 import com.facebook.presto.operator.Description;
 import com.facebook.presto.operator.scalar.ScalarFunction;
+import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.type.SqlType;
 import io.airlift.slice.Slice;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
+
+import static java.util.concurrent.TimeUnit.DAYS;
 
 
 /**
@@ -20,9 +25,8 @@ import java.util.Calendar;
 public class DayOfWeekFunction {
     public final static DateTimeFormatter DEFAULT_DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd");
 
-
     @ScalarFunction("dayOfWeek")
-    @Description("Returns the day of week from a date string")
+    @Description("Returns the day of week from a date string(yyyy-MM-dd)")
     @SqlType(StandardTypes.INTEGER)
     public static long dayOfWeek(@SqlType(StandardTypes.VARCHAR) Slice string) {
         if (string == null) {
@@ -30,8 +34,8 @@ public class DayOfWeekFunction {
         }
 
         try {
-            LocalDate date = LocalDate.parse(string.toStringUtf8(), DEFAULT_DATE_FORMATTER);
-            return date.getDayOfWeek();
+            LocalDate localDate = LocalDate.parse(string.toStringUtf8(), DEFAULT_DATE_FORMATTER);
+            return localDate.getDayOfWeek();
         } catch (Exception e) {
             return -1;
         }
@@ -40,12 +44,12 @@ public class DayOfWeekFunction {
     @ScalarFunction("dayOfWeek")
     @Description("Returns the day of week from a date string")
     @SqlType(StandardTypes.INTEGER)
-    public static long dayOfWeek(@SqlType(StandardTypes.DATE) long t) {
+    public static long dayOfWeek(@SqlType(StandardTypes.DATE) long date) {
         try {
             Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(t);
-            LocalDate date = LocalDate.fromCalendarFields(calendar);
-            return date.getDayOfWeek();
+            calendar.setTimeInMillis(DAYS.toMillis(date));
+            LocalDate localDate = LocalDate.fromCalendarFields(calendar);
+            return localDate.getDayOfWeek();
         } catch (Exception e) {
             return -1;
         }
